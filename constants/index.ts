08675c10c4467b84a129746bb7aef1635d272bf4
Varsha-Shrotriya <varshashrotriya808@gmail.com>
2024-04-15 10:53:39 +0530

    rfefre

---
+++ constants/index.ts
@@ -0,0 +1,84 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/', key: 'overview', label: 'Overview' },
+  { href: '/', key: 'features', label: 'Features' },
+  { href: '/', key: 'payment_status ', label: 'Payment Status ' },
+  { href: '/', key: 'contact_us', label: 'Contact Us' },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  '/person-1.png',
+  '/person-2.png',
+  '/person-3.png',
+  '/person-4.png',
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: 'Expense Tracking',
+    icon: '/map.svg',
+    variant: 'green',
+    description:
+      'Allow users to categorize and track their expenses, including recurring bills, one-time purchases, and discretionary spending',
+  },
+  {
+    title: 'Budget Management',
+    icon: '/calendar.svg',
+    variant: 'green',
+    description:
+      "Enable users to set budget limits for different expense categories and receive notifications when they exceed predefined thresholds.",
+  },
+  {
+    title: 'Savings Goal Tracking',
+    icon: '/tech.svg',
+    variant: 'green',
+    description:
+      'Allow users to set savings goals and track their progress over time, with visual indicators such as pie charts or bar graphs on the basis of monthly and daily tracking and milestone notifications.',
+  },
+  {
+    title: 'Transaction Analysis',
+    icon: '/location.svg',
+    variant: 'orange',
+    description:
+      'Provide insights into spending patterns and trends through interactive charts and graphs, including monthly expenditure breakdowns and category-wise comparisons',
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: 'Learn More',
+    links: [
+      'Magazine',
+      'Blog',
+      'Community',
+      'Scoring and Ranking',
+    ],
+  },
+  {
+    title: 'Our Community',
+    links: ['In the news', 'Events', 'Financial Infographics'],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Contact Us',
+  links: [
+    { label: 'Admin Officer', value: '123-456-7890' },
+    { label: 'Email Officer', value: 'expanses@tracking.com' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Social',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg',
+    '/youtube.svg',
+    '/wordpress.svg',
+  ],
+};
